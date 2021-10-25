f5322047ccfec560be07c6435ea69f913c587a2e
Lip <meebitlip@gmail.com>
2021-10-25 01:05:39 -0400

    working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-const { protocol, session } = require("electron");
 const { menubar } = require("menubar");
-// const { fetch } = require("electron-fetch").default;
-const path = require("path");
-// const remote = require('electron').remote;
-// const fs = require('fs');
 
 const mb = menubar({
   dir: __dirname,
@@ -12,18 +7,15 @@ const mb = menubar({
 });
 
 mb.on("ready", () => {
-  const { net } = require("electron");
   const content = mb.window.webContents;
   try {
     content.debugger.attach("1.3");
   } catch (err) {
     console.log("Debugger attach failed: ", err);
   }
-
   content.debugger.on("detach", (event, reason) => {
     console.log("Debugger detached due to: ", reason);
   });
-
   content.debugger.on("message", (event, method, params) => {
     if (method === "Network.responseReceived") {
       if (
@@ -38,7 +30,6 @@ mb.on("ready", () => {
           .then((response) => {
             const body = response.body;
             console.log(body.slice(0, 20));
-            // const res = JSON.parse(body);
             if (body) {
               const res = JSON.parse(body);
               mb.tray.setTitle(
@@ -50,41 +41,13 @@ mb.on("ready", () => {
       }
     }
   });
-
   content.debugger.sendCommand("Network.enable");
-  // protocol.interceptBufferProtocol("https", (request, result) => {
-  //   if (request.url === "http://www.google.com")
-  //     return result(content);
-  // });
-  // session.defaultSession.webRequest.onBeforeRequest(
-  //   { urls: ["https://etherscan.io/autoUpdateGasTracker.ashx?*"] },
-  //   (details, cb) => {
-  //     console.log("URL", details.url);
-  //     // if (!details.url.endsWith("&lipapp=true")) {
-  //     //   const request = net.request(`${details.url}&lipapp=true`);
-  //     //   console.log(request);
-  //     //   request.on("response", (response) => {
-  //     //     console.log(`STATUS: ${response.statusCode}`);
-  //     //     console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-  //     //     response.on("data", (chunk) => {
-  //     //       console.log(`BODY: ${chunk}`);
-  //     //     });
-  //     //     response.on("end", () => {
-  //     //       console.log("No more data in response.");
-  //     //     });
-  //     //   });
-  //     //   request.end();
-  //     // }
-  //   }
-  // );
-  // console.log(content);
   content.reload();
-  // setInterval(content.reload, 1000);
+  setInterval(content.reload, 1000);
   content.on("did-finish-load", () => {
     console.log("load");
-    // content.insertCSS(
-    //   fs.readFileSync(path.join(__dirname, "inject.css"), "utf8")
-    // );
-    content.insertCSS("header,footer {display: none!important;}", { cssOrigin: "user" });
+    content.insertCSS("header,footer {display: none!important;}", {
+      cssOrigin: "user",
+    });
   });
 });
